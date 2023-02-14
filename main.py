599e5acc4c35f3aa370e867255948a3a637ccebf
Jeffrey Scott <valenpendragon@gmail.com>
2023-02-14 07:14:37 -0500

    Complete web page display of APOD data.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import functions
 response = functions.get_apod_data()
 img_info = response["explanation"]
 img_url = response["url"]
+img_title = response["title"]
 if "copyright" in response.keys():
     copyright_info = response["copyright"]
 else:
@@ -14,14 +15,14 @@ image_get = functions.get_image_data(img_url)
 # Begin building web page.
 st.set_page_config(layout="wide")
 
-col1 = st.columns(1)
+# col1 = st.columns(1)
 
-with col1:
-    st.title("NASA's Astronomy Picture of the Day")
-    if image_get:
-        st.image("images/today.jpg")
-    else:
-        st.write("Image could not be retrieved today.")
-    st.info(img_info)
-    if copyright_info:
-        st.write(f"Image Copyright: {copyright_info}")
+st.title("NASA's Astronomy Picture of the Day")
+st.title(img_title)
+if image_get:
+    st.image("images/today.jpg")
+else:
+    st.write("Image could not be retrieved today.")
+st.info(img_info)
+if copyright_info:
+    st.write(f"Image Copyright: {copyright_info}")
